f9d2f426aef693934d2c9789b1c24876df24668a
JulieRodriguez2016 <rodriguezjulie34@gmail.com>
2016-11-23 20:12:10 -0700

    Update word_bank1.java

--- word_bank1.java
+++ word_bank1.java
@@ -4,9 +4,9 @@ public class word_bank1 extends javax.swing.JFrame {
  
 public static void main(String[] args){
 String words = "engineering:software:testing:compile:protected:private:"+
-                       "debug:inheritance:import:semicolon:char:boolean:integer:commenting"+
-                       "statistic:coding:umldiagram:array:static:class:void:main:jframe:random"+
-                       "generator:system:scanner:loops:statements:program:index:sorting:method"+
+                       "debug:inheritance:import:semicolon:char:boolean:integer:commenting:"+
+                       "statistic:coding:umldiagram:array:static:class:void:main:jframe:random:"+
+                       "generator:system:scanner:loops:statements:program:index:sorting:method:"+
                        "private:slider:java:eclipse:netbeans:jgrasp";
 String[] wordsAsArray = words.split(":");
 
